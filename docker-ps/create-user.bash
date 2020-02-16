bb29bfaad029ee46ae1f3dde074e8ce5eba8c2ce
Pavel Svoboda <pavel.sa@gmail.com>
2020-02-16 17:38:10 +0000

    Change container spin-up
    Relates to --gpus not supported in docker-compose yet

--- docker-ps/create-user.bash
+++ docker-ps/create-user.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-: ${LOGIN:="developer"}
-: ${USER_UID:=1000}
-: ${USER_SHELL:="/bin/bash"}
-: ${USER_HOME:="/home/$LOGIN"}
+: "${LOGIN:="developer"}"
+: "${USER_UID:=1000}"
+: "${USER_SHELL:="/bin/bash"}"
+: "${USER_HOME:="/home/$LOGIN"}"
 
 useradd --uid $USER_UID \
         --non-unique \
@@ -14,7 +14,13 @@ useradd --uid $USER_UID \
 
 chown $LOGIN $USER_HOME -R
 
-echo "$LOGIN ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$LOGIN \
-   && chmod 0440 /etc/sudoers.d/$LOGIN
+echo "$LOGIN ALL=(ALL:ALL) NOPASSWD: ALL" >/etc/sudoers.d/$LOGIN &&
+        chmod 0440 /etc/sudoers.d/$LOGIN
 
-exec gosu $LOGIN "$@"
+# Temporary fixed due to vscode docker remote based on docker image.
+# This should be dropped when --gpus get supported in docker-compose.yml
+# Now the --gpus can be forwarded vie run-args only, see devcontainer.json.
+parameters=("$@")
+((${#parameters[@]})) && COMMAND=("$@") || COMMAND=("/bin/sh" "-c" "sleep infinity")
+
+exec gosu $LOGIN "${COMMAND[@]}"
